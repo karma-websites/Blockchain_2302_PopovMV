9b45889503da7c226cedba02e196c428855608f4
karma-websites <67168746+karma-websites@users.noreply.github.com>
2025-12-25 00:55:15 +0300

    Implement factorial generation function
    Added a function to generate the factorial of a number.

--- tasks/Task_03.sol
+++ tasks/Task_03.sol
@@ -15,5 +15,16 @@ contract Task_03 {
 
    function getUser(uint _index) external view returns (string memory name, uint8 age) {
        return (people[_index].name, people[_index].age);
-    }
+   }
+
+
+   function generateFactorial(uint256 n) external pure returns (uint256) {
+       uint256 result = 1;
+       uint256 i = 1;
+       while (i <= n) {
+          result *= i;
+          i++;
+       }
+       return result;
+   }
 }

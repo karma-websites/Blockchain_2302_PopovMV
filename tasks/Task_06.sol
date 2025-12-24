2cdb661db2adf94b73457c004642321385de2afe
karma-websites <67168746+karma-websites@users.noreply.github.com>
2025-12-25 01:02:11 +0300

    Add MAX_UINT8 constant to Task_06 contract

--- tasks/Task_06.sol
+++ tasks/Task_06.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 contract Task_06 {
    uint256 public constant MAX_UINT = type(uint256).max; // Максимальное значение uint256
+   uint8 public constant MAX_UINT8 = type(uint8).max;
 
 
    function getMaxUint256() external pure returns (uint256) {

976cf2f7a81f0d5304f469eedb8f0078c401e11d
karma-websites <67168746+karma-websites@users.noreply.github.com>
2025-12-25 01:05:04 +0300

    Refactor comments in Task_13.sol for clarity
    Updated comments for clarity and consistency in the ExampleUsage contract.

--- tasks/Task_13.sol
+++ tasks/Task_13.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract ExampleUsage {
-    // 1) Пример использования assert:
-    // Проверка должна основываться на том, что переданное значение НЕ равно 0
+    // 1) Пример использования assert
     function assertExample(uint256 _value) public pure {
+        // Проверка должна основываться на том, что переданное значение должно быть не равно 0
         assert(_value != 0);
     }
 
-    // 2) Пример использования revert:
-    // При ложном переданном значении выполнять revert
+    // 2) Пример использования revert
     function revertExample(bool _condition) public pure {
-        if (_condition == false) {
+        // Проверка должна основываться на том, что при ложном переданном значении выполнять revert
+        if (!_condition) {
             revert("Invalid condition");
         }
     }
 
-    // 3) Пример использования if-else:
-    // else должен возвращать "Value is 10 or less"
+    // 3) Пример использования if-else
     function ifElseExample(uint256 _value) public pure returns (string memory) {
+        // Закончить оператор if конструкцией else. В else возвращать "Value is 10 or less"
         if (_value > 10) {
             return "Value is greater than 10";
         } else {
@@ -26,9 +26,10 @@ contract ExampleUsage {
         }
     }
 
-    // 4) Пример использования else if:
-    // Проверить и исправить логические условия
+    // 4) Пример использования else if
     function elseIfExample(uint256 _value) public pure returns (string memory) {
+        // Проверяем и исправляем логические условия:
+        // >10, ==10, иначе <10
         if (_value > 10) {
             return "Value is greater than 10";
         } else if (_value == 10) {

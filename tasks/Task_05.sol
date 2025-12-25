43898a70d190879be56fc7db588b1c0cb2d36169
karma-websites <67168746+karma-websites@users.noreply.github.com>
2025-12-25 03:20:31 +0300

    Refactor getAllFahrenheitTemperatures function

--- tasks/Task_05.sol
+++ tasks/Task_05.sol
@@ -28,11 +28,12 @@ contract Task_05 {
       celsiusTemperatures.push(_celsius);
    }
 
-    function getAllFahrenheitTemperatures() external view returns (int256[] memory) {
-        int256[] memory fahrenheitTemperatures = new int256[](celsiusTemperatures.length);
-        for (uint256 i = 0; i < celsiusTemperatures.length; i++) {
-            fahrenheitTemperatures[i] = (celsiusTemperatures[i] * 9) / 5 + 32;
-        }
-        return fahrenheitTemperatures;
-    }
+
+   function getAllFahrenheitTemperatures() external view returns (int256[] memory) {
+      int256[] memory fahrenheitTemperatures = new int256[](celsiusTemperatures.length);
+      for (uint256 i = 0; i < celsiusTemperatures.length; i++) {
+         fahrenheitTemperatures[i] = (celsiusTemperatures[i] * 9) / 5 + 32;
+      }
+      return fahrenheitTemperatures;
+   }
 }
